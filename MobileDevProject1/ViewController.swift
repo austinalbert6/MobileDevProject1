1e7334b31ad038e9475d65e86bb66b312a4f0633
Austin Albert <ala0086@auburn.edu>
2025-01-19 10:36:55 -0600

    Initial Commit

---
+++ MobileDevProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileDevProject1
+//
+//  Created by Austin Albert on 1/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
